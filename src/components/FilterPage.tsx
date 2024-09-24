d9326c43218abec4e414a0308f177020e1d13a2a
Navin grg <grgnabin60@gmail.com>
2024-09-24 14:21:16 +0545

    Added filter Page

--- src/components/FilterPage.tsx
+++ src/components/FilterPage.tsx
@@ -1,10 +1,57 @@
 import React from 'react'
+import { Button } from './ui/button'
+import { Checkbox } from './ui/checkbox';
+import { Label } from './ui/label';
 
-type Props = {}
+export interface IFilterOptions {
+  id: string,
+  label: string
+};
 
-const FilterPage = (props: Props) => {
+const filterOptions: IFilterOptions[] = [
+  { 
+    id: "burger",
+    label: "Burger"
+  },
+  { 
+    id: "thali",
+    label: "Thali"
+  },
+  { 
+    id: "biriyani",
+    label: "Biriyani"
+  },
+  { 
+    id: "momos",
+    label: "Momos"
+  },
+];
+
+
+const FilterPage:React.FC = () => {
+
+  const appliedFilterHandler = (val: string) => {
+
+  }
+
   return (
-    <div>FilterPage</div>
+    <div className='md:w-72'>
+      <div className='flex items-center justify-between'>
+        <h1 className='font-medium text-lg'>Filter by cuisines</h1>
+        <Button variant={'link'}>Reset</Button>
+      </div>
+        {
+          filterOptions.map((x: IFilterOptions) => (
+            <div key={x.id} className='flex items-center space-x-2 my-5'>
+              <Checkbox 
+                id={x.id}
+                onClick={() => appliedFilterHandler(x.label)}
+              />
+              <Label className='tetx-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'>{x.label}</Label>
+            </div>
+          ))
+        }
+    </div>
   )
 }
 
